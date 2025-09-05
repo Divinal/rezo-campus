c8f8b8cb2528ced797b1a0ef580e4c1fb04d29b5
mvouama-divin <divinmister@gmail.com>
2025-09-05 17:55:58 +0100

    Chargement about

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -117,11 +117,10 @@ const Blog: React.FC = () => {
     return (
       <div className="min-h-screen flex flex-col">
         <Header />
-        <main className="flex-grow bg-gray-50 py-8">
-          <div className="container mx-auto px-4">
-            <div className="max-w-6xl mx-auto text-center">
-              <p>Chargement des contenus...</p>
-            </div>
+        <main className="flex-grow flex items-center justify-center">
+          <div className="text-center">
+            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary mx-auto mb-4"></div>
+            <p className="text-gray-600">Chargement des contenus...</p>
           </div>
         </main>
         <Footer />
